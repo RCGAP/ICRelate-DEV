4ed8e42c2d09426847f8164cf788247f87067da6
RCGAP <Garnetangeldust@gmail.com>
2022-01-23 16:32:36 +0100

    role admin & user

--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Post;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class PostCrudController extends AbstractCrudController
@@ -12,14 +14,14 @@ class PostCrudController extends AbstractCrudController
         return Post::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('title'),
-            TextEditorField::new('description'),
+            TextEditorField::new('introduction'),
+            TextField::new('content'),
         ];
     }
-    */
+    
 }
